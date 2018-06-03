49ff620351981e7f5010954e96934d7f606aa7f5
John O'Sullivan <josullivan1@luc.edu>
2018-06-02 21:54:24 -0500

    Added testing with mocha

---
+++ tests/users/users.test.js
@@ -0,0 +1,9 @@
+const expect = require('expect');
+const request = require('supertest');
+
+// POST /users
+describe('POST /users', () => {
+  it('should create a user with valid parameters', (done) => {
+    done();
+  });
+});
